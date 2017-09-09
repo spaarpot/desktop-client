56d1e8f0e6871474e5a4efe38d078a33d4e3a213
Thomas Prochazka <thomas.prochazka@gmail.com>
2017-09-09 12:59:44 +0200

    Add first implementation of MainArea with dummy Transactions

--- src/app/storage/model.ts
+++ src/app/storage/model.ts
@@ -2,3 +2,11 @@ export class Account {
     title: string;
     balance?: number;
 }
+
+export class Transaction {
+    creationDate: Date;
+    payee: string;
+    category: string;
+    notes: string;
+    amount: number;
+}
