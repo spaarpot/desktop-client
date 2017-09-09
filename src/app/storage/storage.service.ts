669ca977e248c9e1433c157922018bbb231807fe
Alexander Falb <fal3xx@gmail.com>
2017-09-09 14:48:32 +0200

    replace inline dummy data with an actual file loader

--- src/app/storage/storage.service.ts
+++ src/app/storage/storage.service.ts
@@ -1,47 +1,38 @@
 import { Injectable } from '@angular/core';
-import { Account, Transaction } from './model';
+import { Account, Transaction, Savefile } from './model';
+import * as fs from 'fs';
 
 @Injectable()
 export class StorageService {
 
-    DUMMY_ACCOUNTS: Account[] = [
-        { title: 'Versicherungen', balance: 238.98 },
-        { title: 'Tanken', balance: -47.3 },
-        { title: 'Arbeit Essen' },
-        { title: 'Sport', balance: -19.99 },
-        { title: 'Fortgehen' },
-        { title: 'Videospiele' },
-        { title: 'Motorrad', balance: 89.93 }
-    ];
+    private savefileName: string;
+    private savefile: Savefile;
 
-    DUMMY_CATEGORIES: Account[] = [
-        { title: 'Sport', balance: -19.99 },
-        { title: 'Fortgehen' }
-    ];
+    getAccounts = (): Account[] => {
+        return this.savefile.accounts;
+    };
 
-    DUMMY_TRANSACTIONS: Transaction[] = [
-        { creationDate: new Date(), payee: 'BMW Heiligenstadt', category: 'Auto Reperatur', notes: 'Rücklicht', amount: -159.3 },
-        { creationDate: new Date(), payee: 'BMW Heiligenstadt', category: 'Auto Reperatur', notes: 'Rücklicht', amount: -159.3 },
-        { creationDate: new Date(), payee: 'BMW Heiligenstadt', category: 'Auto Reperatur', notes: 'Rücklicht', amount: -159.3 },
-        { creationDate: new Date(), payee: 'BMW Heiligenstadt', category: 'Auto Reperatur', notes: 'Rücklicht', amount: -159.3 },
-        { creationDate: new Date(), payee: 'BMW Heiligenstadt', category: 'Auto Reperatur', notes: 'Rücklicht', amount: -159.3 },
-        { creationDate: new Date(), payee: 'BMW Heiligenstadt', category: 'Auto Reperatur', notes: 'Rücklicht', amount: -159.3 },
-    ];
+    getCategories = (): Account[] => {
+        return this.savefile.categories;
+    };
 
-    constructor() {
-    }
+    getTransactions = (): Transaction[] => {
+        return this.savefile.transactions;
+    };
 
-    getAccounts(): Account[] {
-        return this.DUMMY_ACCOUNTS;
-    }
+    openFile = (filename: string): Promise<void> => {
+        return new Promise((resolve, reject) => {
+            fs.readFile(filename, 'utf8', (err, data) => {
+                if (err) {
+                    console.warn('An error occurred opening the file ' + err.message);
+                    reject(err);
+                } else {
+                    this.savefileName = filename;
+                    this.savefile = JSON.parse(data);
+                    resolve();
+                }
+            });
+        });
+    };
 
-    // TODO: Type is currently Account, until there is a Category/Pot/whatever class that shares a common superclass with account.
-    // TODO: If this is not the same as above, we would need 2 directives for displaying them. Maybe this is also a way to go.
-    getCategories(): Account[] {
-        return this.DUMMY_CATEGORIES;
-    }
-
-    getTransactions(): Transaction[] {
-        return this.DUMMY_TRANSACTIONS;
-    }
 }
