db4a3fd9c89fe9bf0eded77fa4d65c4dbf251e35
Thomas Prochazka <thomas.prochazka@gmail.com>
2017-10-18 22:48:39 +0200

    Fix initial error on click of 'New File' or 'Open File'

--- src/app/store/save.metareducer.ts
+++ src/app/store/save.metareducer.ts
@@ -1,5 +1,5 @@
 import { ActionReducer } from '@ngrx/store';
-import { FileLoaded, FILE_LOADED } from '../actions/application.actions';
+import { FileLoaded, FILE_LOADED, NEW_FILE, OPEN_FILE } from '../actions/application.actions';
 import { writeAppState } from './fileaccess';
 
 
@@ -7,12 +7,17 @@ export function saveReducer(reducer: ActionReducer<any>): ActionReducer<any> {
     return function (state, action) {
         console.log('action state', action, state);
 
-        if (action.type === FILE_LOADED) {
-            return reducer((action as FileLoaded).payload, action);
-        } else {
-            const newState = reducer(state, action);
-            writeAppState(newState).subscribe();
-            return newState;
+        switch (action.type) {
+            case FILE_LOADED:
+                return reducer((action as FileLoaded).payload, action);
+            case NEW_FILE:
+            case OPEN_FILE:
+                // Ignore, nothing to save yet
+                return state;
+            default:
+                const newState = reducer(state, action);
+                writeAppState(newState).subscribe();
+                return newState;
         }
     };
 }
